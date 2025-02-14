f2bbb11a2b6d92d750923e46e93e5fa6a1eea5cc
xavimrg <javier.martin.mr@gmail.com>
2025-02-14 16:27:18 +0100

    5 try

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { SinglePageComponent } from './layout/single-page/single-page.component';
 
 export const routes: Routes = [
-{   path: '',
-    component: SinglePageComponent,}
+    { path: '', component: SinglePageComponent, pathMatch: 'full' },
+    { path: '**', redirectTo: '', pathMatch: 'full' } 
 ];
